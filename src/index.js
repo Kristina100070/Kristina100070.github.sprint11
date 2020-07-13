4756286f4d008a6477b9002d0d633f920945e5a7
Kristina Gromova <christina28romova@yandex.ru>
2020-07-13 17:11:28 +0300

    update

--- src/index.js
+++ src/index.js
@@ -10,16 +10,19 @@ import {CardPopup} from './js/CardPopup';
 import {PopupAvatar} from './js/PopupAvatar';
 
 const list = document.querySelector('.places-list');
-
 const popupCardElement = document.querySelector('.popup__card');
 const popupEditElement = document.querySelector('.popup__edit');
 const popupAvatarElement = document.querySelector('.popup__avatar');
-
-
 const buttonPopupEditOpen = document.querySelector('.button__edit');
 const buttonPopupCardOpen = document.querySelector('.user-info__button');
-
 const popupButtonAvatar = document.querySelector('.user-info__photo');
+const formCard = document.querySelector('.popup__form_card');
+const formCardName = document.querySelector('.popup__card_name');
+const formCardLink = document.querySelector('.popup__card_link');
+const formInfo = document.querySelector('.popup__form_info');
+const formInfoName = document.querySelector('.popup__input_type_nameinfo');
+const formInfoJob = document.querySelector('.popup__input_type_job');
+const formAvatar = document.querySelector('.popup__form_avatar');
 
 const card = new Card();
 const popupCard = new CardPopup(popupCardElement, document.forms.new);
@@ -28,12 +31,7 @@ buttonPopupCardOpen.addEventListener('click', function (event) {
   popupCard.open();
 });
 
-
-const formCard = document.querySelector('.popup__form_card');
-const formCardName = document.querySelector('.popup__card_name');
-const formCardLink = document.querySelector('.popup__card_link');
-
-formCard.addEventListener('submit', function (event) {
+document.querySelector('.popup__button_card').addEventListener('click', function (event) {
   event.preventDefault();
   api.setCard(formCardName.value, formCardLink.value)
  .then (res => cardList.addCard(res));
@@ -46,11 +44,7 @@ buttonPopupEditOpen.addEventListener('click', function (event) {
   userInfo.open();
 });
 
-const formInfo = document.querySelector('.popup__form_info');
-const formInfoName = document.querySelector('.popup__input_type_nameinfo');
-const formInfoJob = document.querySelector('.popup__input_type_job');
-
-formInfo.addEventListener('submit', function (event) {
+document.querySelector('.popup__button_info').addEventListener('click', function (event) {
   event.preventDefault();
   api.updateProfile(formInfoName.value, formInfoJob.value)
   .then(res => userInfo.updateUserInfo(res));
@@ -59,7 +53,7 @@ formInfo.addEventListener('submit', function (event) {
 
 const validateFormCard = new FormValidator(formCard);
 const validateFormEdit = new FormValidator(formInfo);
-const validateFormAvatar = new FormValidator(document.forms.avatar);
+const validateFormAvatar = new FormValidator(formAvatar);
 validateFormCard.setEventListeners();
 validateFormEdit.setEventListeners();
 validateFormAvatar.setEventListeners();
@@ -92,15 +86,16 @@ cardList.setCards(initialCards);
 cardList.render();
   })
 
-  const popupAvatar = new PopupAvatar(popupAvatarElement, document.forms.avatar);
+const popupAvatar = new PopupAvatar(popupAvatarElement, document.forms.avatar);
  
-  popupButtonAvatar.addEventListener('click', function (event) {
-    popupAvatar.open();
-  });
-  document.forms.avatar.addEventListener('submit', function (event) {
-    event.preventDefault();
-    api.updateAvatar(document.forms.avatar.elements.link.value)
+popupButtonAvatar.addEventListener('click', function (event) {
+  popupAvatar.open();
+});
+
+document.querySelector('.popup__button_avatar').addEventListener('click', function (event) {
+  event.preventDefault();
+  api.updateAvatar(document.forms.avatar.elements.link.value)
    .then (res => popupButtonAvatar.style.backgroundImage = `url(${res.avatar})`)
     popupAvatar.close();
-    document.forms.avatar.reset();
-  });  
+    formAvatar.reset();
+});  
